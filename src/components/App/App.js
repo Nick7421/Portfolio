06d8f915bc59c1a022e771046b0ed06fead68593
Nick Neiert <nickster7421@gmail.com>
2019-02-01 16:12:51 -0600

    created base components for Admin page and Portfolio home page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   // Renders the entire app on the DOM
---
+++ src/components/PortfolioHome/PortfolioHome.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class PortfolioHome extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+    export default PortfolioHome;

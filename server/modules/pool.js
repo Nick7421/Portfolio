246f0ac45051ff827d9e56f76e8c2c8d93e6bcc6
Nick Neiert <nickster7421@gmail.com>
2019-02-02 13:21:59 -0600

    added routes to project page and admin page in server.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -23,7 +23,7 @@ if (process.env.DATABASE_URL) {
   config = {
     host: 'localhost', // Server hosting the postgres database
     port: 5432, // env var: PGPORT
-    database: 'giphy_search_favorites', // change this line if you want to use a different database
+    database: 'portfolio', // change this line if you want to use a different database
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
--- server/server.js
+++ server/server.js
@@ -3,12 +3,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
+// Route Includes
+const projectRouter = require('./routes/project.router')
+const adminRouter = require('./routes/admin.router')
+
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-
+app.use('/project', projectRouter);
+app.use('/admin', adminRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {

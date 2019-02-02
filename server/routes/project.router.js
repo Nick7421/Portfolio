6d52cce4d80c6544217eff1288f100ee6f46798f
Nick Neiert <nickster7421@gmail.com>
2019-02-02 13:34:09 -0600

    completed GET route and tested with postman for /project route

--- server/routes/project.router.js
+++ server/routes/project.router.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const pool = require('../modules/pool');
 
-
+router.get('/',(req,res) => {
+    const queryText = 'SELECT * FROM "projects";';
+    pool.query(queryText)
+        .then((result)=>{
+            res.send(result.rows);
+        })
+        .catch( (error) =>{
+            console.log(`error on GET query ${error}`);
+        })
+});
 
 
 

4ec1877b4ade8fbf10a34bef077f84f6f4177793
Nick Neiert <nickster7421@gmail.com>
2019-02-03 10:57:50 -0600

    created map for Portfolio Home to display the PortcolioCards component

--- src/components/PortfolioHome/PortfolioHome.js
+++ src/components/PortfolioHome/PortfolioHome.js
@@ -2,14 +2,30 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
 import Header from '../Header/Header';
+//Material-UI componenets
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 class PortfolioHome extends Component {
+ componentDidMount(){
+     this.getProjects();
+ }
 
+ getProjects = () => {
+     const action = { type: 'GET_PROJECTS' };
+     this.props.dispatch(action);
+ }
 
   render() {
     return (
         <div>
-      <p>Portfolio Home Page</p>
+        <Header />
+        <Grid container spacing={32}>
+            {this.props.reduxStore.projects.map((projects,i) => (
+                <PortfolioCards key={i} projects={projects} />
+            ))}
+
+        </Grid>
         
       </div>
     )

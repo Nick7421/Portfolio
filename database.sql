e05d7025225fccc941b8158a78fb5a9e9a9b633d
Nick Neiert <nickster7421@gmail.com>
2019-02-04 07:56:05 -0600

    updated database SQL with inserts

--- database.sql
+++ database.sql
@@ -16,3 +16,11 @@ CREATE TABLE "projects" (
 
 INSERT INTO "tags" ("name") 
 VALUES ('React'), ('jQuery'), ('Node'), ('SQL'), ('Redux'), ('HTML');
+
+INSERT INTO "projects"("title", "description", "thumbnail", "github", "date_completed", "tag_id") 
+VALUES('Giphy App', 'This is a random Gif generator using the GIPHY API', './../images/giphyAPI.png', 
+ 'https://github.com/Nick7421/giphy-api-activity, '2019-01-30', '1');
+
+ INSERT INTO "projects"("title", "description", "thumbnail", "github", "date_completed", "tag_id") 
+VALUES('Gallery App', 'This is a gallery of my adventures', './../images/AboutMe.png', 
+ 'https://github.com/Nick7421/zaurak-weekend-4-gallery, '2019-01-22', '1');
